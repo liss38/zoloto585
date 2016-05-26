e0901f8d93ebbda3d5adb88ba941b59236ca8061
Max Salnikov <liss38@yandex.ru>
2016-05-26 15:40:45 +0300

    improve the project, добавил новые папки для определённого назначения каждая, скальпфайл теперь может архивировать, добавил описание к папкам в README.md

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
 	less = require('gulp-less'),
 	concat = require('gulp-concat'),
 	cssmin = require('gulp-cssmin'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	zip = require('gulp-zip');
 
 
 var dev_ = 'development/',
@@ -43,4 +44,17 @@ gulp.task('cssmain', ['lessmain'], function () {
 		.pipe(cssmin())
 		.pipe(rename('main.min.css'))
 		.pipe(gulp.dest(dev_css));
+});
+
+
+// ZIP
+var zip_src_1 = 'no_build/zoloto585__catalog2/',
+	zip_files_1 = [zip_src_1 + '**/*.*', '!' + zip_src_1 + 'catalog_builder.css', '!' + zip_src_1 + 'grid.css'],
+	zip_name_1 = 'zoloto585__catalog2.zip',
+	zip_dest_1 = 'archives/';
+
+gulp.task('zip', function () {
+	return gulp.src(zip_files_1)
+		.pipe(zip(zip_name_1))
+		.pipe(gulp.dest(zip_dest_1));
 });
