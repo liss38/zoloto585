3d3f98428d4e24091757ea04afa7622ef10eb326
Max Salnikov <liss38@yandex.ru>
2016-06-01 22:03:35 +0300

    main-navi like a Toster or VK

--- development/js/top_nav.js
+++ development/js/top_nav.js
@@ -1,15 +1,21 @@
 // обработчик хэдер навигации для тач-версии сайта
 $(document).ready(function() {
+	var navModeLikeToster = true;
+
 	$(document).on('click', function (el) {
 		var wanted = el.target,
 			$wanted = $(wanted);
 
 		if($wanted.hasClass('top-sandwich')) { // закрываем меню
 			if($wanted.hasClass('top-sandwich--active')) {
+				if(navModeLikeToster) $('.section').removeClass('section--left_offset_for_nav');
+
 				$wanted.removeClass('top-sandwich--active');
 				$('.top-nav').removeClass('top-nav--active');
 			}
 			else { // открываем меню
+				if(navModeLikeToster) $('.section').addClass('section--left_offset_for_nav');
+
 				$wanted.addClass('top-sandwich--active');
 				$('.top-nav').addClass('top-nav--active');
 			}
