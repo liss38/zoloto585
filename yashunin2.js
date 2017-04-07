31f9dbf9fd1a88412c53ecaebbb4c9d9a840c48a
Max Salnikov <liss38@yandex.ru>
2017-04-07 15:07:11 +0300

    подготовка к переезду на zoloto585gold

--- yashunin2.js
+++
@@ -1,8 +0,0 @@
-$(".msalnikov").owlCarousel({
-            navigation : true,
-            slideSpeed : 300,
-            paginationSpeed : 400,
-            singleItem:true,
-            autoHeight : true,
-            addClassActive : true,
-		});
